3a4f8c731e9e0a507d1f9a5d27677710c0afee25
harma <w0833331@myscc.ca>
2025-11-07 23:12:29 -0500

    completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home';
+import { ProjectsComponent } from './projects/projects';
+import { ResumeComponent } from './resume/resume';
+import { ExperienceComponent } from './experience/experience';
+import { ContactComponent } from './contact/contact';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
