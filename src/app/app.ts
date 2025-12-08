bfbef0ad2e0ceeb4a5c57797487e594ecac3bf03
harma <w0833331@myscc.ca>
2025-12-07 21:45:09 -0500

    completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+import { HoverHighlightDirective } from './shared/hover-highlight';
+import { TitlePrefixPipe } from './shared/title-prefix-pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    DatePipe,
+    TitleCasePipe,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    HoverHighlightDirective,
+    TitlePrefixPipe,
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  appTitle = 'Harmandeep Singh';
+  today = new Date();
+  currentYear = this.today.getFullYear();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-});
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
+}).catch(err => console.error(err));
